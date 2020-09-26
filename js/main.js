6cde03d53603f6ebd9d1ecc8f2a87c597bc284f0
DanylloENL <danylloenl.de@gmail.com>
2020-09-25 22:05:01 -0300

    Adicionando códigos de Array, Dicionário, Condicionais, Laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,63 @@
-var lista = ["maça", "pêra", "laranja"];
-console.log(lista[1]);
-lista.push("uva");
+
+
+
+
+
+/*
+var d = new Date();
+alert(d.getMinutes());
+alert(d.getDay());
+alert(d.getHours());
+*/
+
+/*
+var count;
+for(count = 0; count<=5; count++){
+    alert(count);
+};
+*/
+
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count ++;
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome: "maca", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+/*
+var fruta = {nome: "maca", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.pop();
+//lista.push("uva");
+//console.log(lista[0]);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
 
 //var nome = "Danyllo Luz";
 //var n1 = 5;
